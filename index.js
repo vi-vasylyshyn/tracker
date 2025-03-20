30394d5f6fb4afc479635d8027ce3f1394c261d0
Vitaliia Vasylyshyn <127021941+vi-vasylyshyn@users.noreply.github.com>
2025-03-20 16:10:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-console.log("Tracker script loaded on another site!");
+function findElementContainingTextDeep(selector, text) {
+    return Array.from(document.querySelectorAll(selector)).find(element =>
+        element.innerText.trim().includes(text)
+    );
+}
 
+function sendEvent(eventData) {
+    console.log("Sending event:", eventData);
+}
 
 (function () {
-  console.log("Tracker script loaded from file!");
+  const events = [
+      { selector: '.radioButtonGroup_radioButton__1S1fy', textContent: 'Female', event: 'click' }
+  ];
 
-  const EVENT_NAME = "dailyActivityReport";
-  const today = new Date().toISOString().split("T")[0];
+  if (events.length > 0) {
+      function attachTracking(events) {
+          events.forEach(({ selector, event, textContent }) => {
+            const element = findElementContainingTextDeep(selector, textContent); 
 
-  // Get last sent date
-  const lastSentDate = localStorage.getItem("lastEventSentDate");
+            if (element) {
+                element.addEventListener(event, function (e) {
+                    console.log(e, `Event on element ${selector} with text ${textContent} has been triggered. Send event!`);
+                    sendEvent({ event: 'ElementTriggered', elementText: textContent, timestamp: new Date().toISOString() });
+                });
+            } else {
+                console.log(`Element with text "${textContent}" not found for selector: ${selector}`);
+            }
+          });
+      }
 
-  if (lastSentDate !== today) {
-    sendEvent();
-    localStorage.setItem("lastEventSentDate", today);
-  }
+      attachTracking(events);
 
-  function sendEvent() {
-    const eventData = {
-      event: EVENT_NAME,
-      timestamp: new Date().toISOString(),
-      activity: "User engaged with the app",
-    };
-
-    console.log("Sending event:", eventData);
+      const today = new Date().toISOString().split("T")[0];
+      sendEvent({ event: 'Initialization', timestamp: today });
+  } else {
+      console.log('No events to track.');
   }
 })();
-
